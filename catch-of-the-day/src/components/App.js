b3f8efe7ca7d53bb9b45173c8bfdb7470e622459
Michael Pang <michaelpang@Michaels-MacBook-Pro.local>
2018-07-08 22:14:04 +0200

    Finished part 15

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -3,17 +3,48 @@ import React from "react";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
+import Fish from "./Fish";
+
+import sampleFishes from "../sample-fishes";
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    const fishes = { ...this.state.fishes };
+    fishes[`fish${Date.now()}`] = fish;
+
+    this.setState({
+      fishes
+    });
+  };
+
+  loadSampleFishes = () => {
+    this.setState({
+      fishes: sampleFishes
+    });
+  };
+
   render() {
     return (
       <React.Fragment>
         <div className="catch-of-the-day">
           <div className="menu">
             <Header tagline="Fresh seafood market" />
+            <ul className="fishes">
+              {Object.keys(this.state.fishes).map(key => (
+                <Fish key={key} details={this.state.fishes[key]} />
+              ))}
+            </ul>
           </div>
           <Order />
-          <Inventory />
+          <Inventory
+            addFish={this.addFish}
+            loadSampleFishes={this.loadSampleFishes}
+          />
         </div>
       </React.Fragment>
     );
